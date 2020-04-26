c6d31a889d0c6cac495c1e68bb564df88373b579
guo <guo@guo.com>
2020-04-26 20:14:03 +0800

    very simple DI implementation.

---
+++ src/Injector.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VerySimpleDI
+{
+    public class ServiceDescriptor
+    {
+        public Type ServiceType { get; set; }
+        public Type ImplementationType { get; set; }
+    }
+
+    public class Injector
+    {
+        private List<ServiceDescriptor> _serviceDescriptors = new List<ServiceDescriptor>();
+
+        public void AddTransient<TService, TImplementation>()
+            where TService : class
+            where TImplementation : class, TService
+        {
+            _serviceDescriptors.Add(new ServiceDescriptor
+            {
+                ServiceType = typeof(TService),
+                ImplementationType = typeof(TImplementation)
+            });
+        }
+
+        public T GetInstance<T>()
+        {
+            Type type = typeof(T);
+
+            var instance = this.getInstance(type);
+
+            return (T)instance;
+        }
+
+        private object getInstance(Type type)
+        {
+            var serviceDescriptor = _serviceDescriptors.FirstOrDefault(x => x.ServiceType == type);
+
+            if (serviceDescriptor == null)
+            {
+                throw new ArgumentException($"{type.Name} not exist!");
+            }
+
+            var ctorInfo = serviceDescriptor.ImplementationType.GetConstructors().FirstOrDefault();
+            var prameters = ctorInfo.GetParameters();
+            var paremeterInstanceList = new List<object>();
+
+            foreach (var parameter in prameters)
+            {
+                var parameterInstance = this.getInstance(parameter.ParameterType);
+                paremeterInstanceList.Add(parameterInstance);
+            }
+
+            var instance = ctorInfo.Invoke(paremeterInstanceList.ToArray());
+
+            return instance;
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace VerySimpleDI
+{
+    public interface IService1
+    {
+        void Display();
+    }
+
+    public interface IService2
+    {
+        void Show();
+    }
+
+    public class Service1 : IService1, IDisposable
+    {
+        public void Display()
+        {
+            Console.WriteLine("Service1 displays itself.");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("dispose Service1");
+        }
+    }
+
+    public class Service2 : IService2, IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("dispose Service2");
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Service2 shows itself.");
+        }
+    }
+
+    public interface IService3
+    {
+        void DoSomething();
+    }
+
+    public class Service3 : IService3, IDisposable
+    {
+        private IService1 _service1;
+        private IService2 _service2;
+
+        public Service3(IService1 service1, IService2 service2)
+        {
+            _service1 = service1;
+            _service2 = service2;
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Service3 DoSomething starts");
+            _service1.Display();
+            _service2.Show();
+            Console.WriteLine("Service3 DoSomething ends");
+
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("dispose Service3");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var injector = new Injector();
+            injector.AddTransient<IService1, Service1>();
+            injector.AddTransient<IService2, Service2>();
+            injector.AddTransient<IService3, Service3>();
+
+            var service3 = injector.GetInstance<IService3>();
+            service3.DoSomething();
+        }
+    }
+}
